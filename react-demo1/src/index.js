5a7c76e49b2ecb8ae625be6be766d0431f284c5e
shane <qvefofo@126.com>
2019-08-12 17:30:42 +0800

    第二次提交

--- react-demo1/src/index.js
+++ react-demo1/src/index.js
@@ -1,24 +1,57 @@
-console.log('hi')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const div = createElement('div', createElement('p', createElement('span', 'hi')))
-// const span = createElement('span')
-// div.appendChild(p)
-// p.appendChild(span)
-// span.innerText = '我是一个span'
+// const div = (
+//     React.createElement('div', null,
+//         React.createElement('p', null,
+//             React.createElement('span', null, 'hi')))
+// )
 
-
-document.body.appendChild(div)
-
-
-function createElement(tagName, children) {
-    const element = document.createElement(tagName)
-    if (children) {
-        if (typeof children === 'string') {
-            var childElement = document.createTextNode(children)
-            element.appendChild(childElement)
-        } else {
-            element.appendChild(children)
-        }
-    }
-    return element
+// 先声明再使用
+const Header = (
+    <header>
+        header
+    </header>
+)
+const Header2 = function (props) {
+    return (
+        <header>
+            header {props.name}
+        </header>
+    )
 }
+const Bottom = (
+    <div>
+        Button
+    </div>
+)
+const Bottom2 = function () {
+    const [n, setN] = React.useState(0) // 初始化
+    return (
+        <div>
+            {n}
+            <button onClick={function () {
+                setN(n + 1) // 使用 useState 修改内部值
+            }}>+1</button>
+        </div>
+    )
+}
+
+const div = (
+    <div>
+        {Header}
+        {Header2({ name: 'jack' })}
+        <Header2 name="jacks" />
+        <p>
+            <span>hi</span>
+        </p>
+        <Bottom2 />
+    </div >
+)
+
+
+
+console.log(div) // 虚拟的 element
+
+// document.body.appendChild(div)
+ReactDOM.render(div, document.getElementById('root'))
